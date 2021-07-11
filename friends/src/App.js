f68b1b399c62f352e7b64d9f6d1dad702ac35e8a
thecuriousones <tamaraleonard46@gmail.com>
2021-07-11 14:25:56 -0400

    created react app

---
+++ friends/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      loading...
+    </div>
+  );
+}
+
+export default App;
